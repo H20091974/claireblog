194720b1d6dbff40ccf284fe72b6b5efccf6ad07
zhongnanhuang209074 <zhongnanhuang209074@sohu-inc.com>
2018-01-20 17:40:06 +0800

    修改后台

--- src/main/java/com/claire/controller/AdminController.java
+++ src/main/java/com/claire/controller/AdminController.java
@@ -2,11 +2,14 @@ package com.claire.controller;
 
 import com.claire.mapping.AdminMapper;
 import com.claire.service.MonitorService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by hzn on 2018/1/6.
@@ -21,13 +24,25 @@ public class AdminController {
     @Autowired
     private MonitorService monitorService;
 
+    Gson gson = new Gson();
+
     @GetMapping("/")
     public String index(Model model){
         System.out.println(adminMapper.selectByPw("admin", "21232f297a57a5a743894a0e4a801fc3"));
         model.addAttribute("freeMemory", monitorService.getFreeMemory());
         model.addAttribute("pageId","index");
-        return "admin/ueditor";
+        return "admin/article_publish";
     }
+
+
+    @RequestMapping("/articlepublish")
+    public String article_publish(HttpServletRequest request){
+
+        System.out.println(gson.toJson(request));
+        return "true";
+
+    }
+
 
 
 }
