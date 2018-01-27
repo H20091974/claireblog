27858b3687f07c30bec89413b87dd9796be584a3
claire <claire@localhost>
2018-01-28 00:43:54 +0800

    Change articleList page. Add gallery and babyinfo page.

---
+++ src/main/java/com/claire/controller/BabyController.java
@@ -0,0 +1,41 @@
+package com.claire.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.collections.functors.ForClosure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.claire.domain.Article;
+import com.claire.domain.User;
+import com.claire.mapping.ArticleMapper;
+import com.claire.service.ArticleService;
+import com.claire.service.UserService;
+
+/**
+ * Claire
+ */
+@Controller
+public class BabyController {
+	
+    @GetMapping("/gallery")
+    public String showAllArticle(Model model){
+
+        return "gallery";
+    }
+    
+
+    @GetMapping("/baby")
+    public String showBaby(Model model){
+
+        return "babyInfo";
+    }
+   
+    
+    
+
+}
